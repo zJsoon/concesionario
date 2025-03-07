6deb8ec039e586328418247f9436d4edcc472460
Asier García Caballero <asier.g.c@opendeusto.es>
2025-03-07 16:33:09 +0100

    ADD - Estructura Mantenimiento

--- proyecto_ud/src/utils/mantenimiento/manitenimiento.h
+++ proyecto_ud/src/utils/mantenimiento/manitenimiento.h
@@ -8,6 +8,22 @@
 #ifndef MANTENIMIENTO_MANITENIMIENTO_H_
 #define MANTENIMIENTO_MANITENIMIENTO_H_
 
+#define TAM_FECHA 11
+#define TAM_TIPO 30
+#define TAM_DESCRIPCION 200
 
+typedef struct {
+    int id;
+    int vehiculo_id;
+    char fecha[TAM_FECHA];
+    char tipo[TAM_TIPO];
+    double coste;
+    char descripcion[TAM_DESCRIPCION];
+} Mantenimiento;
+
+typedef struct {
+    Mantenimiento* aMantenimiento;
+    int numMantenimientos;
+} ListaMantenimientos;
 
 #endif /* MANTENIMIENTO_MANITENIMIENTO_H_ */
--- proyecto_ud/src/utils/mantenimiento/mantenimiento.c
+++ proyecto_ud/src/utils/mantenimiento/mantenimiento.c
@@ -1,8 +1,1 @@
-/*
- * mantenimiento.c
- *
- *  Created on: 6 mar 2025
- *      Author: zJson
- */
-
-
+#include "mantenimiento.h"
