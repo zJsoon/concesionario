2d6c9e2a5c98da2d9b26fc014f169aaef3d46584
Asier García Caballero <asier.g.c@opendeusto.es>
2025-03-07 16:33:29 +0100

    ADD - Estructura Translados

--- proyecto_ud/src/utils/translados/translados.c
+++ proyecto_ud/src/utils/translados/translados.c
@@ -1,8 +1,1 @@
-/*
- * translado.c
- *
- *  Created on: 6 mar 2025
- *      Author: zJson
- */
-
-
+#include "translados.h"
--- proyecto_ud/src/utils/translados/translados.h
+++ proyecto_ud/src/utils/translados/translados.h
@@ -1,13 +1,20 @@
-/*
- * translados.h
- *
- *  Created on: 6 mar 2025
- *      Author: zJson
- */
-
 #ifndef TRANSLADOS_TRANSLADOS_H_
 #define TRANSLADOS_TRANSLADOS_H_
 
+#define TAM_FECHA 11
 
+typedef struct {
+    int id;
+    int vehiculo_id;
+    int concesionario_origen_id;
+    int concesionario_destino_id;
+    char fecha_traslado[TAM_FECHA];
+    int responsable_id;
+} Traslado;
+
+typedef struct {
+    Traslado* aTraslado;
+    int numTraslados;
+} ListaTraslados;
 
 #endif /* TRANSLADOS_TRANSLADOS_H_ */
