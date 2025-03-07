cd418ccb88c6192a7163b1d2ae750fd7c59ee7c5
zJson <admin@zjson.dev>
2025-03-07 16:32:26 +0100

    ADD - Estructura Alquiler

--- proyecto_ud/src/utils/alquiler/alquiler.c
+++ proyecto_ud/src/utils/alquiler/alquiler.c
@@ -1,8 +1,1 @@
-/*
- * alquiler.c
- *
- *  Created on: 6 mar 2025
- *      Author: zJson
- */
-
-
+#include "alquiler.h"
--- proyecto_ud/src/utils/alquiler/alquiler.h
+++ proyecto_ud/src/utils/alquiler/alquiler.h
@@ -1,13 +1,19 @@
-/*
- * alquiler.h
- *
- *  Created on: 6 mar 2025
- *      Author: zJson
- */
-
 #ifndef ALQUILER_ALQUILER_H_
 #define ALQUILER_ALQUILER_H_
 
+#define TAM_FECHA 11
 
+typedef struct {
+    int operacion_id;
+    int vehiculo_id;
+    char fecha_inicio[TAM_FECHA];
+    char fecha_fin[TAM_FECHA];
+    double precio_diario;
+} Alquiler;
+
+typedef struct {
+    Alquiler* aAlquiler;
+    int numAlquileres;
+} ListaAlquileres;
 
 #endif /* ALQUILER_ALQUILER_H_ */
