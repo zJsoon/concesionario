b5da5660683429a3a5e02b599c05dfbbed759ed6
Asier "zJson" García <asier.g.c@opendeusto.es>
2025-03-10 11:38:23 +0100

    ADD - ini file, simplificación de inicialización de listas

---
+++ proyecto_ud/src/comp/ini.c
@@ -0,0 +1,29 @@
+#include "ini.h"
+#include "utils/alquiler/alquiler.h"
+#include "utils/audit/audit.h"
+#include "utils/client/client.h"
+#include "utils/concesionario/concesionario.h"
+#include "utils/emp/emp.h"
+#include "utils/mantenimiento/mantenimiento.h"
+#include "utils/operaciones/operaciones.h"
+#include "utils/renting/renting.h"
+#include "utils/translados/translados.h"
+#include "utils/veh/veh.h"
+#include "utils/vent/vent.h"
+
+void iniListas(ListaAlquileres *la, ListaAuditorias *lau, ListaClientes *lc,
+		ListaConce *lcon, ListaEmp *le, ListaMantenimientos *lm,
+		ListaOperaciones *lop, ListaRenting *lr, ListaTraslados *lt,
+		ListaVeh *lv, ListaVent *lvent) {
+	iniLA(la);
+	iniLAU(lau);
+	iniLC(lc);
+	iniLCon(lcon);
+	iniLE(le);
+	iniLM(lm);
+	iniLOP(lop);
+	iniLR(lr);
+	iniLT(lt);
+	iniLV(lv);
+	iniLVent(lvent);
+}
---
+++ proyecto_ud/src/comp/ini.h
@@ -0,0 +1,9 @@
+#ifndef INI_H_
+#define INI_H_
+
+void iniListas(ListaAlquileres *la, ListaAuditorias *lau, ListaClientes *lc,
+		ListaConce *lcon, ListaEmp *le, ListaMantenimientos *lm,
+		ListaOperaciones *lop, ListaRenting *lr, ListaTraslados *lt,
+		ListaVeh *lv, ListaVent *lvent);
+
+#endif /* INI_H_ */
