f2776599413b4acc5f94aeb73fc30247bcb50c2a
Asier "zJson" García <asier.g.c@opendeusto.es>
2025-03-10 11:39:01 +0100

    EDIT - Inicialización de todas las listas

--- proyecto_ud/src/proyecto_ud.c
+++ proyecto_ud/src/proyecto_ud.c
@@ -1,5 +1,33 @@
 #include "menus/menu.h"
 
+#include "comp/ini.h"
+
+#include "utils/alquiler/alquiler.h"
+#include "utils/audit/audit.h"
+#include "utils/client/client.h"
+#include "utils/concesionario/concesionario.h"
+#include "utils/emp/emp.h"
+#include "utils/mantenimiento/mantenimiento.h"
+#include "utils/operaciones/operaciones.h"
+#include "utils/renting/renting.h"
+#include "utils/translados/translados.h"
+#include "utils/veh/veh.h"
+#include "utils/vent/vent.h"
+
 int main() {
+	ListaAlquileres la;
+	ListaAuditorias lau;
+	ListaClientes lc;
+	ListaConce lcon;
+	ListaEmp le;
+	ListaMantenimientos lm;
+	ListaOperaciones lop;
+	ListaRenting lr;
+	ListaTraslados lt;
+	ListaVeh lv;
+	ListaVent lvent;
+
+	iniListas(&la, &lau, &lc, &lcon, &le, &lm, &lop, &lr, &lt, &lv, &lvent);
+
 	mostrarMenuInicio();
 }
