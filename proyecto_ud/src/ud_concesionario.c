99c4936a007ffd58d60c8f53678bf7462e1315fe
Asier "zJson" García <asier.g.c@opendeusto.es>
2025-03-12 11:04:43 +0100

    EDIT - MENU USUARIO READY

--- proyecto_ud/src/ud_concesionario.c
+++ proyecto_ud/src/ud_concesionario.c
@@ -17,7 +17,8 @@
 #include "utils/vent/vent.h"
 
 int main() {
-	int op, op1, op11, op12, op2, op21, op22, op222, op223, op23, op24, op241, op3, op31, op32, verifyEmpleado=1, verifyGerente=1;
+	//int op, op1, op11, op12, op2, op21, op22, op222, op223, op23, op24, op241, op3, op31, op32, verifyEmpleado=1, verifyGerente=1;
+	int op, op1, op11, op12, op2, op3;
 
 	ListaAlquileres la;
 	ListaAuditorias lau;
@@ -37,269 +38,87 @@ int main() {
 		op = mostrarMenuInicio();
 		switch (op) {
 		case 0:
-			printf("Saliendo...");
-			fflush(stdout);
+			printf("Saliendo...\n");
 			break;
-		case 1: // Usuario
-			op1 = mostrarMenuUsuario();
-			switch (op1) {
-			case 0: // Salir
-				printf("Saliendo...");
-				fflush(stdout);
-				break;
-			case 1: // Ver concesionarios
-				op11 = mostrarMenuUsuarioConce();
-				switch (op11) {
-				case 0:
-					printf("Saliendo...");
+		case 1:
+			do {
+				op1 = mostrarMenuUsuario();
+				switch (op1) {
+				case 0: // Salir
+					printf("Saliendo...\n");
 					fflush(stdout);
 					break;
-				case 1: // Visualizar todos
-					break;
-				case 2: // Filtrar por ciudad
-					break;
-				case 3: // Filtrar por marca
-					break;
-				default:
-					printf("Erro! La opción seleccionada no es correcta\n");
-					fflush(stdout);
-					break;
-				}
-				break;
-			case 2: // Ver vehículos
-				op12 = mostrarMenuUsuarioVeh();
-				switch (op12) {
-				case 0:
-					printf("Saliendo...");
-					fflush(stdout);
-					break;
-				case 1: // Visualizar todos
-					break;
-				case 2: // Filtrar por concesionario
-					break;
-				case 3: // Filtrar por marca
-					break;
-				case 4: // Filtrar por modelo
-					break;
-				case 5: // Filtrar por tipo
-					break;
-				case 6: // Filtrar por estado
-					break;
-				default:
-					printf("Erro! La opción seleccionada no es correcta\n");
-					fflush(stdout);
-					break;
-				}
-				break;
-			case 3: // Consultar historial un vehículo
-				break;
-			default:
-				printf("Erro! La opción seleccionada no es correcta\n");
-				fflush(stdout);
-				break;
-			}
-			break;
-		case 2: // Empleado
-			if (verifyEmpleado) {
-				op2 = mostrarMenuEmp();
-				switch (op2) {
-				case 0:
-					printf("Saliendo...");
-					fflush(stdout);
-					break;
-				case 1: // Gestión Clientes
-					op21 = mostrarMenuEmpGest();
-					switch (op21) {
-					case 0:
-						printf("Saliendo...");
-						fflush(stdout);
-						break;
-					case 1: // Añadir Clientes
-						break;
-					case 2: // Modificar Clientes
-						break;
-					case 3: // Eliminar Clientes
-						break;
-					case 4: // Consultar Clientes
-						break;
-					default:
-						printf("Erro! La opción seleccionada no es correcta\n");
-						fflush(stdout);
-						break;
-					}
-					break;
-				case 2: // Operaciones
-					op22 = mostrarMenuEmpOperaciones();
-					switch (op22) {
-					case 0:
-						printf("Saliendo...");
-						fflush(stdout);
-						break;
-					case 1: // Registrar venta
-						break;
-					case 2: // Registrar alquiler
-						op222 = mostrarMenuEmpOperacionesAlquiler();
-						switch (op222) {
+				case 1: // Ver concesionarios
+					do {
+						op11 = mostrarMenuUsuarioConce();
+						switch (op11) {
 						case 0:
-							printf("Saliendo...");
+							printf("Saliendo...\n");
+							fflush(stdout);
 							break;
-						case 1: // Inicio alquiler
+						case 1: // Visualizar todos
+							printf("Visualizar todos los concesionarios.\n");
+							fflush(stdout);
 							break;
-						case 2: // Estado alquiler
+						case 2: // Filtrar por ciudad
+							printf("Filtrar concesionarios por ciudad.\n");
+							fflush(stdout);
 							break;
-						case 3: // Fin alquiler
+						case 3: // Filtrar por marca
+							printf("Filtrar concesionarios por marca.\n");
+							fflush(stdout);
 							break;
 						default:
-							printf("Error! La opción seleccionada no es correcta\n");
+							printf(
+									"Error! La opción seleccionada no es correcta\n");
 							fflush(stdout);
 							break;
 						}
-						break;
-					case 3: // Registrar renting
-						op223 = mostrarMenuEmpOperacionesRenting();
-						switch (op223) {
-						case 0:
-							printf("Saliendo...");
-							break;
-						case 1: // Inicio renting
-							break;
-						case 2: // Estado renting
-							break;
-						case 3: // Fin reinting
-							break;
-						default:
-							break;
-						}
-						break;
-					case 4: // Registrar movimiento coche
-						break;
-					default:
-						printf(
-								"Error! La opción seleccionada no es correcta\n");
-						fflush(stdout);
-						break;
-					}
+					} while (op11 != 0);
 					break;
-				case 3: // Mantenimientos
-					op23 = mostrarMenuEmpMantenimiento();
-					switch (op23) {
-					case 0:
-						printf("Saliendo...");
-						break;
-					case 1: // Registrar reparaciones
-						break;
-					case 2: // Registrar revisiones
-						break;
-					case 3: // Visualizar reparaciones
-						break;
-					case 4: // Visualizar revisiones
-						break;
-					default:
-						printf("Error! La opción seleccionada no es correcta\n");
-						fflush(stdout);
-						break;
-					}
-					break;
-				case 4: // Informes
-					op24 = mostrarMenuEmpInformes();
-					switch (op24) {
-					case 0:
-						printf("Saliendo...");
-						fflush(stdout);
-						break;
-					case 1: // Generar informes
-						op241 = mostrarMenuEmpInformesGenerar();
-						switch (op241) {
+				case 2: // Ver vehículos
+					do {
+						op12 = mostrarMenuUsuarioVeh();
+						switch (op12) {
 						case 0:
-							printf("Saliendo...");
+							printf("Saliendo...\n");
 							fflush(stdout);
 							break;
-						case 1: // Informe venta
+						case 1: // Visualizar todos
+							printf("Visualizar todos los vehículos.\n");
+							fflush(stdout);
 							break;
-						case 2: // Informe alquiler
+						case 2: // Filtrar por concesionario
+							printf("Filtrar por concesionarios\n");
+														fflush(stdout);
 							break;
-						case 3: // Informe renting
+						case 3: // Filtrar por marca
+							printf("Filtrar por marca\n");
+														fflush(stdout);
 							break;
-						case 4: // Informe movimiento coche
+						case 4: // Filtrar por modelo
+							printf("Filtrar por modelo\n");
+														fflush(stdout);
 							break;
-						case 5: // Informe reparaciones
+						case 5: // Filtrar por tipo
+							printf("Filtrar por tipo\n");
+														fflush(stdout);
 							break;
-						case 6: // Informe revisiones
+						case 6: // Filtrar por estado
+							printf("Filtrar por estado\n");
+														fflush(stdout);
 							break;
 						default:
-							printf("Error! La opción seleccionada no es correcta\n");
+							printf(
+									"Error! La opción seleccionada no es correcta\n");
 							fflush(stdout);
 							break;
 						}
-						break;
-					case 2: // Visualizar informes
-						break;
-					default:
-						printf(
-								"Error! La opción seleccionada no es correcta\n");
-						fflush(stdout);
-						break;
-					}
+					} while (op12 != 0);
 					break;
-				default:
-					printf("Erro! La opción seleccionada no es correcta\n");
+				case 3: // Consultar historial un vehículo
+					printf("Consultar historial un vehículo.\n");
 					fflush(stdout);
-					break;
-				}
-				break;
-			}
-			break;
-		case 3: // Gerente
-			if (verifyGerente) {
-				op3 = mostrarMenuGerente();
-				switch (op3) {
-				case 0:
-					printf("Saliendo...");
-					fflush(stdout);
-					break;
-				case 1: // Gestión empleados
-					op31 = mostrarMenuGerenteEmp();
-					switch (op31) {
-					case 0:
-						printf("Saliendo...");
-						fflush(stdout);
-						break;
-					case 1: // Añadir empleado
-						break;
-					case 2: // Modificar empleado
-						break;
-					case 3: // Eliminar empleado
-						break;
-					case 4: // Consultar empleado
-						break;
-					default:
-						printf(
-								"Error! La opción seleccionada no es correcta\n");
-						fflush(stdout);
-						break;
-					}
-					break;
-				case 2: // Gestión concesionarios
-					op32 = mostrarMenuGerenteConce();
-					switch (op32) {
-					case 0:
-						printf("Saliendo...");
-						fflush(stdout);
-						break;
-					case 1: // Añadir concesionarios
-						break;
-					case 2: // Modificar concesionarios
-						break;
-					case 3: // Eliminar concesionarios
-						break;
-					case 4: // consultar concesionarios
-						break;
-					default:
-						printf(
-								"Error! La opción seleccionada no es correcta\n");
-						fflush(stdout);
-						break;
-					}
 					break;
 				default:
 					printf("Error! La opción seleccionada no es correcta\n");
@@ -307,7 +126,21 @@ int main() {
 					break;
 				}
 				break;
-			}
+			} while (op1 != 0);
+			break;
+		case 2:
+			do {
+				op2 = mostrarMenuEmp();
+				printf("Hemos entrado a una categoría de empleado.\n");
+				fflush(stdout);
+			} while (op2 != 0);
+			break;
+		case 3:
+			do {
+				op3 = mostrarMenuGerente();
+				printf("Hemos entrado a una categoria de gerente.\n");
+				fflush(stdout);
+			} while (op3 != 0);
 			break;
 		default:
 			printf("Error! La opción seleccionada no es correcta\n");
@@ -315,6 +148,288 @@ int main() {
 			break;
 		}
 	} while (op != 0);
+	/*do {
+	 op = mostrarMenuInicio();
+	 switch (op) {
+	 case 0:
+	 printf("Saliendo...");
+	 fflush(stdout);
+	 break;
+	 case 1: // Usuario
+	 op1 = mostrarMenuUsuario();
+	 switch (op1) {
+	 case 0: // Salir
+	 printf("Saliendo...");
+	 fflush(stdout);
+	 break;
+	 case 1: // Ver concesionarios
+	 op11 = mostrarMenuUsuarioConce();
+	 switch (op11) {
+	 case 0:
+	 printf("Saliendo...");
+	 fflush(stdout);
+	 break;
+	 case 1: // Visualizar todos
+	 break;
+	 case 2: // Filtrar por ciudad
+	 break;
+	 case 3: // Filtrar por marca
+	 break;
+	 default:
+	 printf("Erro! La opción seleccionada no es correcta\n");
+	 fflush(stdout);
+	 break;
+	 }
+	 break;
+	 case 2: // Ver vehículos
+	 op12 = mostrarMenuUsuarioVeh();
+	 switch (op12) {
+	 case 0:
+	 printf("Saliendo...");
+	 fflush(stdout);
+	 break;
+	 case 1: // Visualizar todos
+	 break;
+	 case 2: // Filtrar por concesionario
+	 break;
+	 case 3: // Filtrar por marca
+	 break;
+	 case 4: // Filtrar por modelo
+	 break;
+	 case 5: // Filtrar por tipo
+	 break;
+	 case 6: // Filtrar por estado
+	 break;
+	 default:
+	 printf("Erro! La opción seleccionada no es correcta\n");
+	 fflush(stdout);
+	 break;
+	 }
+	 break;
+	 case 3: // Consultar historial un vehículo
+	 break;
+	 default:
+	 printf("Erro! La opción seleccionada no es correcta\n");
+	 fflush(stdout);
+	 break;
+	 }
+	 break;
+	 case 2: // Empleado
+	 if (verifyEmpleado) {
+	 op2 = mostrarMenuEmp();
+	 switch (op2) {
+	 case 0:
+	 printf("Saliendo...");
+	 fflush(stdout);
+	 break;
+	 case 1: // Gestión Clientes
+	 op21 = mostrarMenuEmpGest();
+	 switch (op21) {
+	 case 0:
+	 printf("Saliendo...");
+	 fflush(stdout);
+	 break;
+	 case 1: // Añadir Clientes
+	 break;
+	 case 2: // Modificar Clientes
+	 break;
+	 case 3: // Eliminar Clientes
+	 break;
+	 case 4: // Consultar Clientes
+	 break;
+	 default:
+	 printf("Erro! La opción seleccionada no es correcta\n");
+	 fflush(stdout);
+	 break;
+	 }
+	 break;
+	 case 2: // Operaciones
+	 op22 = mostrarMenuEmpOperaciones();
+	 switch (op22) {
+	 case 0:
+	 printf("Saliendo...");
+	 fflush(stdout);
+	 break;
+	 case 1: // Registrar venta
+	 break;
+	 case 2: // Registrar alquiler
+	 op222 = mostrarMenuEmpOperacionesAlquiler();
+	 switch (op222) {
+	 case 0:
+	 printf("Saliendo...");
+	 break;
+	 case 1: // Inicio alquiler
+	 break;
+	 case 2: // Estado alquiler
+	 break;
+	 case 3: // Fin alquiler
+	 break;
+	 default:
+	 printf("Error! La opción seleccionada no es correcta\n");
+	 fflush(stdout);
+	 break;
+	 }
+	 break;
+	 case 3: // Registrar renting
+	 op223 = mostrarMenuEmpOperacionesRenting();
+	 switch (op223) {
+	 case 0:
+	 printf("Saliendo...");
+	 break;
+	 case 1: // Inicio renting
+	 break;
+	 case 2: // Estado renting
+	 break;
+	 case 3: // Fin reinting
+	 break;
+	 default:
+	 break;
+	 }
+	 break;
+	 case 4: // Registrar movimiento coche
+	 break;
+	 default:
+	 printf(
+	 "Error! La opción seleccionada no es correcta\n");
+	 fflush(stdout);
+	 break;
+	 }
+	 break;
+	 case 3: // Mantenimientos
+	 op23 = mostrarMenuEmpMantenimiento();
+	 switch (op23) {
+	 case 0:
+	 printf("Saliendo...");
+	 break;
+	 case 1: // Registrar reparaciones
+	 break;
+	 case 2: // Registrar revisiones
+	 break;
+	 case 3: // Visualizar reparaciones
+	 break;
+	 case 4: // Visualizar revisiones
+	 break;
+	 default:
+	 printf("Error! La opción seleccionada no es correcta\n");
+	 fflush(stdout);
+	 break;
+	 }
+	 break;
+	 case 4: // Informes
+	 op24 = mostrarMenuEmpInformes();
+	 switch (op24) {
+	 case 0:
+	 printf("Saliendo...");
+	 fflush(stdout);
+	 break;
+	 case 1: // Generar informes
+	 op241 = mostrarMenuEmpInformesGenerar();
+	 switch (op241) {
+	 case 0:
+	 printf("Saliendo...");
+	 fflush(stdout);
+	 break;
+	 case 1: // Informe venta
+	 break;
+	 case 2: // Informe alquiler
+	 break;
+	 case 3: // Informe renting
+	 break;
+	 case 4: // Informe movimiento coche
+	 break;
+	 case 5: // Informe reparaciones
+	 break;
+	 case 6: // Informe revisiones
+	 break;
+	 default:
+	 printf("Error! La opción seleccionada no es correcta\n");
+	 fflush(stdout);
+	 break;
+	 }
+	 break;
+	 case 2: // Visualizar informes
+	 break;
+	 default:
+	 printf(
+	 "Error! La opción seleccionada no es correcta\n");
+	 fflush(stdout);
+	 break;
+	 }
+	 break;
+	 default:
+	 printf("Erro! La opción seleccionada no es correcta\n");
+	 fflush(stdout);
+	 break;
+	 }
+	 break;
+	 }
+	 break;
+	 case 3: // Gerente
+	 if (verifyGerente) {
+	 op3 = mostrarMenuGerente();
+	 switch (op3) {
+	 case 0:
+	 printf("Saliendo...");
+	 fflush(stdout);
+	 break;
+	 case 1: // Gestión empleados
+	 op31 = mostrarMenuGerenteEmp();
+	 switch (op31) {
+	 case 0:
+	 printf("Saliendo...");
+	 fflush(stdout);
+	 break;
+	 case 1: // Añadir empleado
+	 break;
+	 case 2: // Modificar empleado
+	 break;
+	 case 3: // Eliminar empleado
+	 break;
+	 case 4: // Consultar empleado
+	 break;
+	 default:
+	 printf(
+	 "Error! La opción seleccionada no es correcta\n");
+	 fflush(stdout);
+	 break;
+	 }
+	 break;
+	 case 2: // Gestión concesionarios
+	 op32 = mostrarMenuGerenteConce();
+	 switch (op32) {
+	 case 0:
+	 printf("Saliendo...");
+	 fflush(stdout);
+	 break;
+	 case 1: // Añadir concesionarios
+	 break;
+	 case 2: // Modificar concesionarios
+	 break;
+	 case 3: // Eliminar concesionarios
+	 break;
+	 case 4: // consultar concesionarios
+	 break;
+	 default:
+	 printf(
+	 "Error! La opción seleccionada no es correcta\n");
+	 fflush(stdout);
+	 break;
+	 }
+	 break;
+	 default:
+	 printf("Error! La opción seleccionada no es correcta\n");
+	 fflush(stdout);
+	 break;
+	 }
+	 break;
+	 }
+	 break;
+	 default:
+	 printf("Error! La opción seleccionada no es correcta\n");
+	 fflush(stdout);
+	 break;
+	 }
+	 } while (op != 0);*/
 
 	freeListas(&la, &lau, &lc, &lcon, &le, &lm, &lop, &lr, &lt, &lv, &lvent);
 }
