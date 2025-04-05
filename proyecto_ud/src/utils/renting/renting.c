829d301382d650463c31123a160fe17527fdfbe8
Declan Garcia-Oubiña <declan.g.s@opendeusto.es>
2025-04-05 11:58:57 +0200

    Implementar funciones de renting: iniciar, consultar estado y finalizar

--- proyecto_ud/src/utils/renting/renting.c
+++ proyecto_ud/src/utils/renting/renting.c
@@ -61,4 +61,66 @@ Renting registrarRenting(ListaRenting lr) {
     scanf("%lf", &r.precio_mensual);
 
     return r;
+
+    // Iniciar un renting (crear uno nuevo para una matrícula)
+    Renting startRenting(ListaRenting *lr, int vehiculo_id) {
+        Renting r;
+
+        r.operacion_id = lr->numRenting;
+        r.vehiculo_id = vehiculo_id;
+
+        printf("Introduce la duración del renting en meses: ");
+        scanf("%d", &r.duracion_meses);
+        printf("Introduce el precio mensual del renting: ");
+        scanf("%lf", &r.precio_mensual);
+
+        addRenting(lr, r);
+
+        printf("Renting iniciado correctamente.\n");
+        return r;
+    }
+
+    // Mostrar el estado del renting de un vehículo
+    void estadoRenting(ListaRenting lr, int vehiculo_id) {
+        int i, encontrado = 0;
+        for (i = 0; i < lr.numRenting; i++) {
+            if (lr.aRenting[i].vehiculo_id == vehiculo_id) {
+                printf("Estado del renting:\n");
+                printR(lr.aRenting[i]);
+                encontrado = 1;
+                break;
+            }
+        }
+        if (!encontrado) {
+            printf("No se encontró un renting activo para el vehículo con ID %d.\n", vehiculo_id);
+        }
+    }
+
+    // Finalizar un renting (eliminarlo)
+    Renting endRenting(ListaRenting *lr, int vehiculo_id) {
+        int i, j;
+        Renting eliminado;
+        eliminado.operacion_id = -1; // indicador de no encontrado
+
+        for (i = 0; i < lr->numRenting; i++) {
+            if (lr->aRenting[i].vehiculo_id == vehiculo_id) {
+                eliminado = lr->aRenting[i];
+
+                for (j = i; j < lr->numRenting - 1; j++) {
+                    lr->aRenting[j] = lr->aRenting[j + 1];
+                }
+                lr->numRenting--;
+                lr->aRenting = realloc(lr->aRenting, lr->numRenting * sizeof(Renting));
+                printf("Renting finalizado y eliminado correctamente.\n");
+                break;
+            }
+        }
+
+        if (eliminado.operacion_id == -1) {
+            printf("No se encontró un renting para el vehículo con ID %d.\n", vehiculo_id);
+        }
+
+        return eliminado;
+    }
+
 }
--- proyecto_ud/src/utils/renting/renting.h
+++ proyecto_ud/src/utils/renting/renting.h
@@ -18,4 +18,8 @@ void addRenting(ListaRenting *lr, Renting r);
 void printR(Renting r);
 void printLR(ListaRenting lr);
 Renting registrarRenting(ListaRenting lr);
+Renting startRenting(ListaRenting *lr, int vehiculo_id);
+void estadoRenting(ListaRenting lr, int vehiculo_id);
+Renting endRenting(ListaRenting *lr, int vehiculo_id);
+
 #endif /* RENTING_RENTING_H_ */
